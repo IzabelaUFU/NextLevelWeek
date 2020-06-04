b8ecfcd22197b44b6d1f5de1eb14a0540347548d
IzabelaUFU <38962331+IzabelaUFU@users.noreply.github.com>
2020-06-04 13:20:15 -0300

    correção/add pagina

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,8 @@ const botaoBusca = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
 const close = document.querySelector("#modal .header a")
 
-buttonSearch.addEventListener("click", ()=>{
-    modal.classList.toggle("hide")
+botaoBusca.addEventListener("click", ()=>{
+    modal.classList.remove("hide")
 })
 close.addEventListener("click", ()=>{
     modal.classList.add("hide")
